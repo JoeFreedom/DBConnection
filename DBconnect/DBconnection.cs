307248c92d97cc8dcb51d74b1d2e6010c98c43af
SergeySamodurovS <god-sergey@yandex.ru>
2020-07-10 21:48:06 +0300

    Initial commit
    connect to DB uses file .cfg

---
+++ DBconnect/DBconnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace DBconnect
+{
+    public class DBconnection
+    {
+        private MySqlConnection connection;
+
+        private readonly string host;
+        private readonly string database;
+        private readonly string port;
+        private readonly string username;
+        private readonly string pass;
+        private readonly string ConnString;
+
+        public DBconnection()
+        {
+            using (var file = new StreamReader("dbconnect.cfg"))
+            {
+                string tempLine;
+                while ((tempLine = file.ReadLine()) != null)
+                {
+                    tempLine = tempLine.Trim();
+                    var index = tempLine.IndexOf('=');
+                    if (index < 0)
+                        continue;
+                    var tempSymbols = tempLine.Substring(index + 1);
+                    var tempVar = tempLine.Substring(0, index);
+                    tempSymbols = tempSymbols.Trim();
+                    tempVar = tempVar.Trim();
+
+                    switch (tempVar)
+                    {
+                        case "host":
+                            host = tempSymbols;
+                            break;
+                        case "database":
+                            database = tempSymbols;
+                            break;
+                        case "port":
+                            port = tempSymbols;
+                            break;
+                        case "username":
+                            username = tempSymbols;
+                            break;
+                        case "pass":
+                            pass = tempSymbols;
+                            break;
+                    }
+                }
+            }
+            ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
+
+            connection = new MySqlConnection(ConnString);
+            connection.Open();
+        }
+    }
+}
