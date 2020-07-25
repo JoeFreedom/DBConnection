2314ecc01ae2123ee347311cd9735b0841202d65
YakovGrechkin2003 <58858243+YakovGrechkin2003@users.noreply.github.com>
2020-07-25 18:21:35 +0300

    Update DBconnection #2
    Добавил конструктор с параметрами

--- DBconnect/DBconnection.cs
+++ DBconnect/DBconnection.cs
@@ -15,6 +15,8 @@ namespace DBconnect
         private readonly string pass;
         private readonly string ConnString;
 
+        public bool isConnect;
+
         public DBconnection()
         {
             using (var file = new StreamReader("dbconnect.cfg"))
@@ -52,10 +54,28 @@ namespace DBconnect
                 }
             }
             ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
+            ConnectDB();
+        }
 
+        public DBconnection(string host, string database, string port, string username, string pass)
+        {
+            ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
+            ConnectDB();
+        }
+
+        public void ConnectDB() 
+        {
             connection = new MySqlConnection(ConnString);
             connection.Open();
+            if(connection.Ping())
+            {
+                isConnect = true;
+            } else
+            {
+                isConnect = false;
+            }
         }
+
         public MySqlDataReader SelectQuery(string sql)
         {
             var command = new MySqlCommand { Connection = connection, CommandText = sql };
